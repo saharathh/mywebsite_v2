a8e9f0627c7f784be68c55d1eca635fc30571d5d
saharathh <saharathjump@gmail.com>
2022-02-16 20:37:37 +0700

    find a way to do css in MUI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, Toolbar, Tabs, Tab } from '@mui/material'
+import './Tabs.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppBar className="appbar">
+      <Toolbar className="tabs">
+        <Tabs>
+          <Tab label="home"></Tab>
+        </Tabs>
+      </Toolbar>
+    </AppBar>
   );
 }
 
